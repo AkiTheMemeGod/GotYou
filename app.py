d0f4b4119368e5126bddb87d2202d5724b268a20
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-01-25 11:52:27 +0530

    Init database and app

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import sqlite3 as sq
+
 
 app = Flask(__name__)
-
+con = sq.connect('database.db')
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
 
 
